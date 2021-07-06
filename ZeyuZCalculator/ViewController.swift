e833a1f7588c33541893514f3c4e1b244c9d2b66
YerikZZY <YerikZZY@users.noreply.github.com>
2021-07-06 14:54:40 -0500

    This is a calculator for addition
    Other calculating rules are easily to imply

--- ZeyuZCalculator/ViewController.swift
+++ ZeyuZCalculator/ViewController.swift
@@ -8,7 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var loadingNum = ""
+    var start = true
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func numPressed(_ sender: UIButton) {
+        let labelNum = Int(displayLabel.text!)
+        displayLabel.text = "\(labelNum! * 10 + Int(sender.titleLabel!.text!)!)"
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        if(start == true){
+            loadingNum = displayLabel.text!
+            displayLabel.text = "0"
+            start = false
+        }
+        else{
+            add()
+            loadingNum = displayLabel.text!
+            displayLabel.text = "0"
+            start = false
+        }
+        
+    }
+    
+    func add(){
+        var result:Int
+        let num1 = Int(loadingNum)
+        let num2 = Int(displayLabel.text!)
+        result = num1! + num2!
+        displayLabel.text = "\(result)"
+        start = true
+    }
+    
+    
+    @IBAction func equalpressed(_ sender: UIButton) {
+        add()
+    }
+    
+    @IBAction func clearpressed(_ sender: UIButton) {
+        loadingNum = "0"
+        displayLabel.text = "0"
+        start = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
